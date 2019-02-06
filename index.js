e1ecec8c027954d0afc4c65fb843d618689be9ec
vaibhavghadge <ghadge.vaibhav09in5004@gmail.com>
2019-02-06 20:57:50 +0530

    beta version with french language

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 module.exports = function(lang){
     if(lang == "es"){
-        console.log('Hola Vaibhav!!!');
-    } else {
-        console.log('Hello Vaibhav!!!');
+        console.log('Hola Vaibhav!');
+    } 
+    else if(lang == "fr"){
+        console.log('Bonjour Vaibhav!');
+    }else {
+        console.log('Hello Vaibhav!');
     }
 }
